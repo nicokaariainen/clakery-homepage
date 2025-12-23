9224c37a792eb3ca04c352f46a2eab2c986cfcbb
Nico Kääriäinen <nico.kaariainen@akamon.fi>
2025-12-23 17:49:04 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <main>
+    <HelloWorld msg="Clakery home page incoming!" />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
