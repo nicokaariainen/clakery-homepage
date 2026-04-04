fc17f59e7394fc788bab77bc0b2855a6396e6df5
Nico Kääriäinen <nico.kaariainen@akamon.fi>
2026-04-04 21:34:24 +0300

    remove old stuff

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,31 +25,6 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue'),
     },
     {
-      path: '/products',
-      name: 'products',
-      component: () => import('../views/ProductsView.vue'),
-    },
-    {
-      path: '/blog',
-      name: 'blog',
-      component: () => import('../views/BlogView.vue'),
-    },
-    {
-      path: '/blog/:slug',
-      name: 'blog-post',
-      component: () => import('../views/BlogPostView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: () => import('../views/ContactView.vue'),
-    },
-    {
       path: '/:pathMatch(.*)*',
       redirect: '/',
     },
